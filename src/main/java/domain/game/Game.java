8f8a11cd97e443847ac0b1befdc4942cbe348eb2
xinri <xinri@xinri.familyds.net>
2019-05-25 00:02:39 +0200

    implemntation of the step

--- src/main/java/domain/game/Game.java
+++ src/main/java/domain/game/Game.java
@@ -1,13 +1,22 @@
 package domain.game;
 
 import domain.tondeuse.Tondeuse;
+import domain.tondeuse.valuetype.DirectionValueType;
+import domain.tondeuse.valuetype.PositionValueType;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Game {
 
-  public Game(int maxX, int maxY) {
+  private final int maxX;
+  private final int maxY;
+  private final List<Tondeuse> listOfTondeuse;
 
+  public Game(int maxX, int maxY) {
+    this.maxX = maxX;
+    this.maxY = maxY;
+    this.listOfTondeuse = new ArrayList<>();
   }
 
   public void execute() {
@@ -15,10 +24,11 @@ public class Game {
   }
 
   public List<Tondeuse> getTondeuseList() {
-    return Collections.emptyList();
+    return this.listOfTondeuse;
   }
 
   public void addTondeuse(int x, int y, String direction, String listOfOrder) {
-
+    listOfTondeuse.add(
+        new Tondeuse(new PositionValueType(x, y, maxX, maxY), new DirectionValueType(direction)));
   }
 }
